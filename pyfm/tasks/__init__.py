ff5a345cdd659cd2bb3884d693ad8a626e20473a
Michael Lynch <michaellynch628@gmail.com>
2026-03-24 09:13:38 -0700

    Remove hadrons_lmi as default job type

--- pyfm/tasks/__init__.py
+++ pyfm/tasks/__init__.py
@@ -2,10 +2,11 @@ from pyfm.tasks import milc
 from pyfm.tasks import grid
 from pyfm.tasks import hadrons
 from pyfm.tasks import contract
-from pyfm.tasks.register import get_task_handler, get_task_key, register_task
+from pyfm.tasks.register import get_task_handler, get_task_key, list_registered_types, register_task
 
 __all__ = [
     "get_task_handler",
     "get_task_key",
+    "list_registered_types",
     "register_task",
 ]
