830e7b22c48013dbbbb02a7dc8d054ed8417b419
Michael Lynch <michaellynch628@gmail.com>
2026-03-26 08:48:10 -0700

    Add additional testing cases

--- test/tasks/test_generate_input.py
+++ test/tasks/test_generate_input.py
@@ -3,7 +3,6 @@ import yaml
 
 from pyfm.nanny import write_input_file
 
-
 HADRONS_CASES = [
     ("lma", "full-lma"),
     ("meson", "meson"),
@@ -15,11 +14,17 @@ HADRONS_CASES = [
 
 @pytest.mark.parametrize("job_step,io_prefix", HADRONS_CASES)
 def test_generate_hadrons_input(
-    tmp_path, monkeypatch, tasks_data_dir, params, assert_xml_equal, job_step, io_prefix
+    tmp_path,
+    monkeypatch,
+    tasks_data_dir,
+    hadrons_params,
+    assert_xml_equal,
+    job_step,
+    io_prefix,
 ):
     monkeypatch.chdir(tmp_path)
 
-    write_input_file(job_step, params, "a", "20")
+    write_input_file(job_step, hadrons_params, "a", "20")
 
     expected_sched = (
         (tasks_data_dir / "schedules" / f"test-{io_prefix}-a.20.sched")
@@ -30,7 +35,34 @@ def test_generate_hadrons_input(
         (tmp_path / "schedules" / f"{io_prefix}-a.20.sched").read_text().splitlines()
     )
     assert actual_sched[0] == expected_sched[0], "Module count mismatch"
-    assert set(actual_sched[1:]) == set(expected_sched[1:]), "Schedule module set mismatch"
+    assert set(actual_sched[1:]) == set(
+        expected_sched[1:]
+    ), "Schedule module set mismatch"
+
+    assert_xml_equal(
+        tmp_path / "in" / f"{io_prefix}-a.20.xml",
+        tasks_data_dir / "in" / f"test-{io_prefix}-a.20.xml",
+    )
+
+
+GRID_CASES = [
+    ("lma", "grid-full-lma"),
+]
+
+
+@pytest.mark.parametrize("job_step,io_prefix", GRID_CASES)
+def test_generate_grid_input(
+    tmp_path,
+    monkeypatch,
+    tasks_data_dir,
+    grid_params,
+    assert_xml_equal,
+    job_step,
+    io_prefix,
+):
+    monkeypatch.chdir(tmp_path)
+
+    write_input_file(job_step, grid_params, "a", "20")
 
     assert_xml_equal(
         tmp_path / "in" / f"{io_prefix}-a.20.xml",
